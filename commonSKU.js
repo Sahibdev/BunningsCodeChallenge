9d8ad107c740644f621ad42b184689733a824922
Sahibdev <sahibdev.mann@yahoo.com>
2021-07-13 10:06:47 +1000

    feat(commonSKU): avoid exceptions

--- commonSKU.js
+++ commonSKU.js
@@ -2,8 +2,8 @@ export const getCommonSKU = (companyA, companyB) => {
   let set = new Set();
   companyA.map((barcodeA) => {
     companyB.find((barcodeB) => {
-      if (barcodeB.Barcode === barcodeA.Barcode) {
-        set.add(barcodeB.SKU);
+      if (barcodeB?.Barcode && barcodeB?.Barcode === barcodeA?.Barcode) {
+        set.add(barcodeB?.SKU);
         return true;
       } else return false;
     });
--- commonSKU.test.js
+++ commonSKU.test.js
@@ -42,3 +42,39 @@ const expectedSet = new Set(['three']);
 test('Should get SKU of common product', () => {
   expect(getCommonSKU(jsonA, jsonB)).toEqual(expectedSet);
 });
+
+const jsonExpectionA = [
+  {
+    SKU: 'one',
+    Supplier: 'A1',
+  },
+  {
+    SKU: 'two',
+    Supplier: 'AB1',
+  },
+  {
+    SKU: 'three',
+    Supplier: 'BC1',
+  },
+];
+
+const jsonExpectionB = [
+  {
+    SKU: 'one',
+    Supplier: 'B1',
+  },
+  {
+    SKU: 'two',
+    Supplier: 'BB1',
+  },
+  {
+    SKU: 'three',
+    Supplier: 'BC1',
+  },
+];
+
+const expectedExpectionSet = new Set();
+
+test('Should get SKU of common product', () => {
+  expect(getCommonSKU(jsonExpectionA, jsonExpectionB)).toEqual(expectedExpectionSet);
+});
